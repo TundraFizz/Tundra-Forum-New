a03768895fa89d6e0fdb409ee9e03c29ddcbb12c
Tundra Fizz <coleman.leif@gmail.com>
2018-05-08 16:08:56 -0700

    Update utility file to easily make GET and POST requests

--- src/post.js
+++ src/post.js
@@ -1,12 +1,20 @@
 export function post(api, data){
   return new Promise((resolve) => {
     fetch(api, {
-      "method":"POST",
+      "method" : "POST",
+      "body"   : JSON.stringify(data),
       "headers": {
         "Accept": "application/json",
         "Content-Type": "application/json",
-      },
-      "body": JSON.stringify(data)
+      }
       }).then(res => resolve(res.json()));
+  });
+}
+
+export function get(api, data){
+  return new Promise((resolve) => {
+    fetch(api, {
+      "method": "GET"
+    }).then(res => resolve(res.json()));
   });
 }
